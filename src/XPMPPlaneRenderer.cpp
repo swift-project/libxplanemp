f3cb28de1c0a3bab5f02bdf9a95c24f36f927593
TwinFan <twinfan@sonux.net>
2019-02-03 23:13:26 +0100

    SSAA scaling hack only if HDR is on

--- src/XPMPPlaneRenderer.cpp
+++ src/XPMPPlaneRenderer.cpp
@@ -91,6 +91,7 @@ static XPLMDataRef		viewportRef = nullptr;
 bool					gMSAAHackInitialised = false;
 static XPLMDataRef  	gMSAAXRatioRef = nullptr;
 static XPLMDataRef		gMSAAYRatioRef = nullptr;
+static XPLMDataRef      gHDROnRef = nullptr;
 
 static void
 init_cullinfo()
@@ -333,6 +334,7 @@ void			XPMPDefaultPlaneRenderer(int is_blend)
 		gMSAAHackInitialised = true;
 		gMSAAXRatioRef = XPLMFindDataRef("sim/private/controls/hdr/fsaa_ratio_x");
 		gMSAAYRatioRef = XPLMFindDataRef("sim/private/controls/hdr/fsaa_ratio_y");
+		gHDROnRef      = XPLMFindDataRef("sim/graphics/settings/HDR_on");
 	}
 
 	cull_info_t			gl_camera;
@@ -713,11 +715,13 @@ void			XPMPDefaultPlaneRenderer(int is_blend)
 		{
 			float	x_scale = 1.0;
 			float	y_scale = 1.0;
-			if (gMSAAXRatioRef) {
-				x_scale = XPLMGetDataf(gMSAAXRatioRef);
-			}
-			if (gMSAAYRatioRef) {
-				y_scale = XPLMGetDataf(gMSAAYRatioRef);
+			if (gHDROnRef && XPLMGetDatai(gHDROnRef)) {     // SSAA hack only if HDR enabled
+				if (gMSAAXRatioRef) {
+					x_scale = XPLMGetDataf(gMSAAXRatioRef);
+				}
+				if (gMSAAYRatioRef) {
+					y_scale = XPLMGetDataf(gMSAAYRatioRef);
+				}
 			}
 
 			GLint	vp[4];
